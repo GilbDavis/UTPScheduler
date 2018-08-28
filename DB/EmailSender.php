787e7179f1de2d475044fd472afa2c689042bdbe
Gilberto Davis <peter01@hotmail.es>
2018-08-27 19:07:31 -0500

    Redirections for the email send test

--- DB/EmailSender.php
+++ DB/EmailSender.php
@@ -7,30 +7,16 @@
     
     $conn = new mysqli($server, $user, $pass, $db) or die("<p>No se pudo conectar</p>");
     
-    $result = $conn->query("SELECT * FROM Usuarios");
-    echo '<label for="Selectemail">Correos electronicos: </label>';
-    echo '<select id="correosmysql">\n';
-    echo '<option value ="0">Correos...</option>';
-    while($row = $result->fetch_assoc()){
-        echo '<option value="' . $row['id_usuario'] . '">' . $row['email'] . "</option>";
-    }
-    echo "</select>";
-    
-    $conn->close();
-    
     //Codigo para enviar correo electronico
     //Para realizar esto se debe modificar el mail function en php.ini
     // y en sendemail.ini
     
-    /*if(isset($_POST["enviar"])){
+    if(isset($_POST["enviar"])){
         $correo = filter_input(INPUT_POST, "correos");
         $asunto = filter_input(INPUT_POST, "asunto");
         $mensaje = filter_input(INPUT_POST, "mensaje");
         
         mail($correo, $asunto, $mensaje);
-    }*/
-
-
+    }
 
 ?>
-
