4cbf5e302d5ddf40ba49618e184911b87aae892b
elreybob <elreybob@users.noreply.github.com>
2018-10-20 20:49:19 -0500

    Completado el codigo de insercion de recordatorio y mejor de css en cuadros de menu admin

--- Email_Section/GuardarRecordatorio.php
+++ Email_Section/GuardarRecordatorio.php
@@ -1,5 +1,5 @@
 <?php
-
+  session_start();
   require '../Conexion/Connection.php';
 
   //Creacion de variables
@@ -17,27 +17,64 @@
       $repetir = filter_input(INPUT_POST, 'Repetir');
 
       if ($repetir) {
-          //Este pedazo de codigo le resta 1 dia a la fecha insertada
-        $fecha_anterior = new DateTime($fecha);
-        $intervalo = new DateInterval('P1D');
-        $fecha_anterior->sub($intervalo);
-        $fecha_anterior = $fecha_anterior->format('Y-m-d H:i:s');
-        //Cambia la hora de la fecha al predeterminado por el laboratorio que es 
-        //a las 8:00 AM
-        $fecha_repeticion = new DateTime($fecha);
-        $fecha_repeticion->setTime(8, 00, 00);
-        $fecha_repeticion = $fecha_repeticion->format('Y-m-d H:i:s');
-        
-        $sql = 'INSERT INTO correos(correo) VALUES ("' . $correo . '");';
-        $sql .= 'INSERT INTO fechas(fecha_principal, fecha_repeticion, fecha_anterior) '
-                . 'VALUES ("' . $fecha . '", "' . $fecha_repeticion . '", "' .
-                $fecha_anterior . '");';
-        $sql .= '';
-        
-        if($conn->multi_query($sql)){
-            
-        }
-      }else{
-          
+          try {
+              //Este pedazo de codigo le resta 1 dia a la fecha insertada
+              $fecha_anterior = new DateTime($fecha);
+              $intervalo = new DateInterval('P1D');
+              $fecha_anterior->sub($intervalo);
+              $fecha_anterior = $fecha_anterior->format('Y-m-d H:i:s');
+              //Cambia la hora de la fecha al predeterminado por el laboratorio que es
+              //a las 8:00 AM
+              $fecha_repeticion = new DateTime($fecha);
+              $fecha_repeticion->setTime(8, 00, 00);
+              $fecha_repeticion = $fecha_repeticion->format('Y-m-d H:i:s');
+
+              $sql1 = 'INSERT INTO correos(correo) VALUES ("' . $correo . '");';
+              $db1 = $conn->query($sql1);
+              $idcorreos = $conn->insert_id;
+
+              $sql2 = 'INSERT INTO fechas(fecha_principal, fecha_repeticion, fecha_anterior) '
+                    . 'VALUES ("' . $fecha . '", "' . $fecha_repeticion . '", "' .
+                    $fecha_anterior . '");';
+              $db2 = $conn->query($sql2);
+              $idfechas = $conn->insert_id;
+
+              $sql3 = 'INSERT INTO notificacion(id_usuario, id_asunto, id_fechas,'
+                    . ' id_estado, id_correos, mensaje) VALUES (' . $_SESSION['user_id'] .
+                    ', ' . $asunto . ', ' . $idfechas . ', 1, ' . $idcorreos . ', "' . $mensaje . '");';
+              $db3 = $conn->query($sql3);
+
+              if ($db3) {
+                  echo '<script language="javascript">';
+                  echo 'alert("Guardado con exito!");';
+                  echo '</script>';
+              }
+          } catch (Exception $ex) {
+              echo $ex->getMessage();
+          }
+      } else {
+          try {
+              $sql1 = 'INSERT INTO correos(correo) VALUES ("' . $correo . '");';
+              $db1 = $conn->query($sql1);
+              $idcorreos = $conn->insert_id;
+
+              $sql2 = 'INSERT INTO fechas(fecha_principal) '
+                    . 'VALUES ("' . $fecha . '");';
+              $db2 = $conn->query($sql2);
+              $idfechas = $conn->insert_id;
+
+              $sql3 = 'INSERT INTO notificacion(id_usuario, id_asunto, id_fechas,'
+                    . ' id_estado, id_correos, mensaje) VALUES (' . $_SESSION['user_id'] .
+                    ', ' . $asunto . ', ' . $idfechas . ', 1, ' . $idcorreos . ', "' . $mensaje . '");';
+              $db3 = $conn->query($sql3);
+
+              if ($db3) {
+                  echo '<script language="javascript">';
+                  echo 'alert("Guardado con exito!");';
+                  echo '</script>';
+              }
+          } catch (Exception $ex) {
+              echo $ex->getMessage();
+          }
       }
   }
